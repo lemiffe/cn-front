063199c661677f6c655db42175e5bb13332efe81
Elias Meire <me@eliasmei.re>
2018-02-10 19:19:57 +0100

    Fix test

--- src/common/App.test.js
+++ src/common/App.test.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import MemoryRouter from 'react-router-dom/MemoryRouter';
+import configureStore from './store/configureStore';
 
 describe('<App />', () => {
   test('renders without exploding', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-      <Provider>
+      <Provider store={configureStore()}>
         <MemoryRouter>
           <App />
         </MemoryRouter>
