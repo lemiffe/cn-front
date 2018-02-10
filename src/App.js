942809ecbc73432436e1f1357b1ea367a506d042
Arnaud Schlupp <arnaud.schlupp@showpad.com>
2018-02-10 17:48:51 +0100

    Add comments

--- src/App.js
+++ src/App.js
@@ -54,6 +54,75 @@ class App extends Component {
             </span>
           </div>
         </header>
+        {/* comments... */}
+        <main className="o-main">
+          <div className="u-breakpoint o-main__container">
+            <section className="o-content">
+              <ul>
+                <Post
+                  title={
+                    "It's time for DEX. Cybex ICO Launches Shortly - 2018 will be the year of the decentralized exchange."
+                  }
+                  vote={10}
+                  url={
+                    'https://www.reddit.com/r/Iota/comments/7oqlxc/iota_was_introduced_in_mrs_pengs_speech_in_huawei/'
+                  }
+                  domain={'r/Iota'}
+                  comment={'20'}
+                  author={{ name: 'Roy Hollander', title: 'King Of Fighter' }}
+                  published_at={'2018-01-07T14:38:31.146847+00:00'}
+                />
+                {/* Register / Login box */}
+                <li>
+                  <div className="o-comment-form">
+                    <div className="m-textarea">
+                      <textarea />
+                    </div>
+                    <button className="m-button m-button--m m-button--main">
+                      <span className="m-button__label">Comment</span>
+                    </button>
+                  </div>
+                </li>
+              </ul>
+              <ul className="o-comments">
+                <li>
+                  <div>Up</div>
+                  <div>
+                    <div>Username - Time ago - expand or not</div>
+                    <p>Comment</p>
+                    <a>Reply</a>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <textarea />
+                  </div>
+                  <button>Comment</button>
+                </li>
+                <ul className="o-comments">
+                  <li>
+                    <div>Up</div>
+                    <div>
+                      <div>Username - Time ago - expand or not</div>
+                      <p>Comment</p>
+                      <a>Reply</a>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <textarea />
+                    </div>
+                    <button>Comment</button>
+                  </li>
+                </ul>
+              </ul>
+            </section>
+            <aside className="o-sidebar">
+              {/* We will need this... trust me. */}
+            </aside>
+          </div>
+        </main>
+        {/* Posts... */}
         <main className="o-main">
           <div className="u-breakpoint o-main__container">
             <section className="o-content">
@@ -97,7 +166,7 @@ class App extends Component {
               <footer className="o-content__footer">
                 <a
                   href="#"
-                  className="m-button m-button--m m-button--alpha"
+                  className="m-button m-button--m m-button--main"
                   onClick={() => {
                     this.state.page - 1 >= 1
                       ? this.setState(prevState => {
@@ -113,7 +182,7 @@ class App extends Component {
                 <span />
                 <a
                   href="#"
-                  className="m-button m-button--m m-button--alpha"
+                  className="m-button m-button--m m-button--main"
                   onClick={() => {
                     this.setState(prevState => {
                       return {
@@ -172,7 +241,7 @@ class App extends Component {
               </ul>
               <footer className="o-sidebar__footer">
                 <button
-                  className="m-button m-button--m m-button--alpha"
+                  className="m-button m-button--m m-button--main"
                   onClick={() => {
                     this.setState(prevState => {
                       return {
