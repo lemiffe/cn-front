86f0ef02ce4b7d2f01bb8f34cde003e92f4adc6c
Arnaud Schlupp <arnaud.schlupp@showpad.com>
2018-02-11 16:40:20 +0100

    Fix missing dep.

--- src/common/components/PostListItem/PostListItem.js
+++ src/common/components/PostListItem/PostListItem.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Now } from '../Now/Now';
+import { ButtonVote } from '../ButtonVote';
 import { Link } from 'react-router-dom';
 
 export const PostListItem = ({ post, ...rest }) => (
   <li className="o-news" {...rest}>
     <article className="o-news__container">
-      <button className="o-news__vote u-reset-button m-button--4">
-        <i className="material-icons">arrow_drop_up</i>
-        <span>{post.vote}</span>
-      </button>
+      <ButtonVote vote={post.vote} />
       <div>
         <div>
           <a href={post.url} className="u-reset-link o-news__title">
