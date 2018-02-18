20be7ab26a102ba3dce3d7bc1582135a5c6a087b
Arnaud Schlupp <arnaud.schlupp@showpad.com>
2018-02-18 17:11:16 +0100

    WIP add new spec.

--- src/common/pages/CreatePost/CreatePost.js
+++ src/common/pages/CreatePost/CreatePost.js
@@ -13,10 +13,50 @@ export class CreatePost extends PureComponent {
         <Header />
         <main className="o-main">
           <div className="u-breakpoint o-main__container">
-            TODO: CreatePost{' '}
-            <span role="img" aria-label="Magic">
-              ✨
-            </span>
+            <section className="o-content">
+              <div className="o-content__create">
+                <form class="m-form">
+                  <label class="m-form__label">Type</label>
+                  <p class="m-form__detail">
+                    You can post a link or some text...
+                  </p>
+                  <div>
+                    <label className="m-radio">
+                      <input type="radio" checked />
+                      <div className="m-radio__indicator"></div>
+                      <div className="m-radio__label">Link</div>
+                    </label>
+                    <label className="m-radio">
+                      <input type="radio" />
+                      <div className="m-radio__indicator"></div>
+                      <div className="m-radio__label">Text</div>
+                    </label>
+                  </div>
+                  <label class="m-form__label">Link</label>
+                  <p class="m-form__detail">Link only bitches...</p>
+                  <div className="m-input m-input--s m-input--1">
+                    <input type="url" />
+                  </div>
+                  {/* <label class="m-form__label">Text</label>
+                  <p class="m-form__detail">Put some text on here please...</p>
+                  <div className="m-textarea">
+                    <textarea />
+                  </div> */}
+                  <label class="m-form__label">Currency</label>
+                  <p class="m-form__detail">
+                    Select the currency your news is talking about...
+                  </p>
+                  <div>Select currency...</div>
+                  <button className="m-button m-button--m m-button--main">
+                    Add a new speculation
+                  </button>
+                </form>
+              </div>
+              {/* We will need this... trust me. */}
+            </section>
+            <aside className="o-sidebar">
+              {/* We will need this... trust me. */}
+            </aside>
           </div>
         </main>
         <Footer />
